2d56235da76e92b0427066e116a4f97beb7f828a
chk-mm <145430485+chk-mm@users.noreply.github.com>
2024-03-15 02:07:16 +0700

    Delete src directory

--- src/main/java/com/harbourspace/tracker/user/UserRepository.java
+++
@@ -1,4 +0,0 @@
-package com.harbourspace.tracker.user;
-
-public interface UserRepository {
-}
--- src/main/java/com/harbourspace/tracker/user/model/NewUser.java
+++
@@ -1,9 +0,0 @@
-package com.harbourspace.tracker.user.model;
-
-public record NewUser(
-        String name
-) {
-    public User toUser(long id) {
-        return new User(id, name);
-    }
-}
